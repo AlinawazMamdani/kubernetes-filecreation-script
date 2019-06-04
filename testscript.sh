aa70e76aa5d8232879df10c8a6fcfe107d9bc78d
AlinawazMamdani <49072096+AlinawazMamdani@users.noreply.github.com>
2019-06-04 11:14:27 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-counter=0
+counter=100
 
 my_array=(mongo mongo-service secret-service aes-encyption-service session-token-service account-service dashboard-service email-service authentification-service role-service group-service) 
 
-until [ $counter -gt 11 ]
+until [ $counter -gt 111 ]
 do
   echo ${my_array[$counter]}
   name=${my_array[$counter]}
